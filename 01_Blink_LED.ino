ee2bae5107a802020251ada52c70e1fcbe0ee2fa
a-maze-1ng <105156333+a-maze-1ng@users.noreply.github.com>
2023-01-03 20:27:14 +0100

    Update 01_Blink_LED.ino

--- 01_Blink_LED.ino
+++ 01_Blink_LED.ino
@@ -1,15 +1,16 @@
-// the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
 }
 
-// the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // Arduino: turn the LED on (HIGH)
-                                     // D1 Mini: turns the LED *off*
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // Arduino: turn the LED off (LOW)
-                                     // D1 Mini: turns the LED *on*
-  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.print("HIGH");
+  Serial.println();
+  delay(1000);
+  
+  digitalWrite(LED_BUILTIN, LOW);
+  Serial.print("LOW");
+  Serial.println();
+  delay(1000);
 }
